6dfb8dafe2267ce84493e05a2d6c2133051c6f43
Kourou <gaetan.aka.kourou@hotmail.fr>
2023-11-27 21:54:35 -0500

    handle error

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,21 @@ import { Table } from './components/table/page'
 export default function Home() {
 
   const [donations, setDonations] = useState<DonationResponse>([]);
+  const [error, setError] = useState(null);
   useEffect(() => {
     fetch(`http://localhost:3000/api/donations`)
       .then((response) => response.json())
       .then((data: {data: DonationResponse}) => {
-        console.log(data.data)
         setDonations(data.data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => setError(error));
   }, []);
 
   console.log(donations)
 
   return (
-    <Table donations={donations} />
+    error ? 
+      (<>Une erreur est survenue</>) :
+      (<Table donations={donations} />)    
   );
 }
