14ddc2f43177603f9cfeca7bcd2273def4fcaef0
Kourou <gaetan.aka.kourou@hotmail.fr>
2023-11-27 21:54:09 -0500

    reorganize css

--- app/components/table/page.tsx
+++ app/components/table/page.tsx
@@ -1,12 +1,10 @@
 import { DonationResponse } from "@/types";
 
-import styles from './styles.module.css'
-
 export const Table = ({donations}: {donations: DonationResponse}) => {
   return (
     <table>
       <thead>
-        <tr className={styles.tr}>
+        <tr>
           <th>Date</th>
           <th>Prenom</th>
           <th>Nom</th>
@@ -15,11 +13,11 @@ export const Table = ({donations}: {donations: DonationResponse}) => {
       </thead>
       <tbody>
       {donations.map((donation) => (
-        <tr className={styles.tr} key={donation.id}>
-          <td className={styles.td}>{donation.donation.createdAtUtc}</td>
-          <td className={styles.td}>{donation.donation.firstName}</td>
-          <td className={styles.td}>{donation.donation.lastName}</td>
-          <td className={styles.td}>{donation.donation.amount}</td>
+        <tr key={donation.id}>
+          <td>{donation.donation.createdAtUtc}</td>
+          <td>{donation.donation.firstName}</td>
+          <td>{donation.donation.lastName}</td>
+          <td>{donation.donation.amount}</td>
         </tr>
       ))}
       </tbody>
