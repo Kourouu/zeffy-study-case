594397ccc1d6bbcb18b3ed0b40b34788407d355d
Kourou <gaetan.aka.kourou@hotmail.fr>
2023-11-28 08:16:04 -0500

    fix layout

--- app/components/table/row/page.tsx
+++ app/components/table/row/page.tsx
@@ -3,23 +3,11 @@ import { formatDate } from "@/utils";
 
 export const Row = ({donation}: {donation: Donation}) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Prenom</th>
-          <th>Nom</th>
-          <th>Montant</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{formatDate(donation.donation.createdAtUtc)}</td>
-          <td>{donation.donation.firstName}</td>
-          <td>{donation.donation.lastName}</td>
-          <td>{donation.donation.amount}</td>
-        </tr>
-      </tbody>
-    </table>
+    <tr>
+      <td>{formatDate(donation.donation.createdAtUtc)}</td>
+      <td>{donation.donation.firstName}</td>
+      <td>{donation.donation.lastName}</td>
+      <td>{donation.donation.amount}</td>
+    </tr>
   );
 }
